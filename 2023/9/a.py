e3a7093bc980bf81a19bbf232fe85099f5035a95
JCAvila2 <juankmilo1102@hotmail.com>
2024-01-03 01:20:29 -0500

    Day 9 - 2023

---
+++ 2023/9/a.py
@@ -0,0 +1,27 @@
+original_input = open("input.txt", "r").readlines()
+
+output = 0
+
+def extrapolate(numbers):
+    is_zeros = True
+    for n in numbers:
+        if n != 0:
+            is_zeros = False
+            break
+    
+    if is_zeros:
+        return 0
+    else:
+        deltas = []
+        for x in range(len(numbers) - 1):
+            deltas.append(numbers[x + 1] - numbers[x])
+
+        diff = extrapolate(deltas)
+        return diff + numbers[-1]
+    
+
+for n in range(len(original_input)):
+    nums = list(map(int, original_input[n].split()))
+    output += extrapolate(nums)
+
+print(output)
